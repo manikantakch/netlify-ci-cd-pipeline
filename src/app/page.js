e6438945564cb64d0866f49a600c428c1a9b4fad
manikantakch <manikanta.k@narayanagroup.com>
2025-03-19 15:37:38 +0530

    button and input added

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,20 @@
+"use client";
 
+import Button from "@/components/button";
+import Input from "@/components/input";
+import { useState } from "react";
 
 export default function Home() {
+  const [inputValue, setInputValue] = useState("");
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       Home page netlify
+      <Button>Click me!</Button>
+      <Input
+        value={inputValue}
+        placeholder="Enter value"
+        onChange={(e) => setInputValue(e.target.value)}
+      />
     </main>
   );
 }
